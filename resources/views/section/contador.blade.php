83ad640e079824158b58783df597b394246482c8
persontechnology <persontechnologys@gmail.com>
2023-04-12 12:51:07 -0500

    end hss

--- resources/views/section/contador.blade.php
+++ resources/views/section/contador.blade.php
@@ -5,7 +5,7 @@
              <div class="counter-box">
                 <div class="icon"><i class="fal fa-layer-group"></i></div>
                 <div class="counter-content">
-                   <span class="counter" data-count="+" data-to="500" data-speed="1000">1000</span>
+                   <span class="counter" data-count="+" data-to="1000" data-speed="1000">1000</span>
                    <h6 class="title">+ Horas trabajadas</h6>
                 </div>
              </div>
@@ -14,7 +14,7 @@
              <div class="counter-box mt-40">
                 <div class="icon"><i class="fal fa-mug-hot"></i></div>
                 <div class="counter-content">
-                   <span class="counter" data-count="+" data-to="50" data-speed="100">100</span>
+                   <span class="counter" data-count="+" data-to="100" data-speed="100">100</span>
                    <h6 class="title">+ Clientes coorporativos</h6>
                 </div>
              </div>
@@ -23,7 +23,7 @@
              <div class="counter-box">
                 <div class="icon"><i class="fal fa-user-friends"></i></div>
                 <div class="counter-content">
-                   <span class="counter" data-count="+" data-to="2000" data-speed="3000">3000</span>
+                   <span class="counter" data-count="+" data-to="3000" data-speed="3000">3000</span>
                    <h6 class="title">+ Personas certificadas </h6>
                 </div>
              </div>
@@ -32,7 +32,7 @@
              <div class="counter-box mt-40">
                 <div class="icon"><i class="fal fa-award"></i></div>
                 <div class="counter-content">
-                   <span class="counter" data-count="+" data-to="25" data-speed="50">50</span>
+                   <span class="counter" data-count="+" data-to="50" data-speed="50">50</span>
                    <h6 class="title">+  Premios</h6>
                 </div>
              </div>
